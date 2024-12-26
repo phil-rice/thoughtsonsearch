a54f0d59f1bcd9ee06dee40d25545222ec497972
Phil <phil.rice@validoc.org>
2024-12-26 17:21:22 +0000

    displaying errors in search, and passing state to search

--- modules/react/sovereign/sovereign_search/src/advance.search.sovereign.page.tsx
+++ modules/react/sovereign/sovereign_search/src/advance.search.sovereign.page.tsx
@@ -1,4 +1,4 @@
-import {makeSovereignStatePlugin, useSelectedSovereign} from "@enterprise_search/sovereign";
+import {makeSovereignStatePlugin} from "@enterprise_search/sovereign";
 import {useSearchBar} from "@enterprise_search/search_bar";
 import {useDisplayAllFilters} from "@enterprise_search/react_filters_plugin";
 import React, {useEffect} from "react";
@@ -8,7 +8,7 @@ import {DataViewNavBar} from "@enterprise_search/data_views";
 import {keywordsFilterName} from "@enterprise_search/react_keywords_filter_plugin";
 import {useFiltersByStateType} from "@enterprise_search/react_search_state";
 import {useSearchParser} from "@enterprise_search/react_search_parser";
-import {dataViewFilterName, DataViewFilters} from "@enterprise_search/react_data_views_filter_plugin";
+import {DataViewFilters} from "@enterprise_search/react_data_views_filter_plugin";
 
 
 export function AdvancedSearchSovereignPage<Filters extends DataViewFilters>() {
@@ -17,8 +17,8 @@ export function AdvancedSearchSovereignPage<Filters extends DataViewFilters>() {
     const [immediateFilters, setImmediateFilters] = useFiltersByStateType<Filters>('immediate')
     const parser = useSearchParser()
     const filterOps = useGuiFilters()
-    const guiFilters: Filters = filterOps[0]
-    const [searchQuery]= useGuiSearchQuery()
+    const [guiFilters, setGuiFilters] = filterOps
+    const [searchQuery] = useGuiSearchQuery()
     const {DisplayAllFilters} = useDisplayAllFilters<Filters>()
 
     const immediateSearch = (searchQuery: string) => {
@@ -26,12 +26,11 @@ export function AdvancedSearchSovereignPage<Filters extends DataViewFilters>() {
         setImmediateFilters(parser(newGuiFilters, immediateFilters))
     }
     const mainSearch = () => {
-        const newGuiFilters = {...guiFilters, [keywordsFilterName]: searchQuery}
-        setMainFilters(parser(newGuiFilters, mainFilters));
+        setGuiFilters(parser({...guiFilters, [keywordsFilterName]: searchQuery}, guiFilters));
     };
     useEffect(() => {
-        mainSearch()
-    }, [guiFilters[dataViewFilterName]])
+        setMainFilters(parser(guiFilters, mainFilters))
+    }, [guiFilters])
 
     return <>
         <h1>Advance search</h1>
